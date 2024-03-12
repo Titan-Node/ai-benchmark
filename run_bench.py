58e76f66e0b3d4ad9f2a377666d380508149fa2b
Titan Node <hello@titan-node.com>
2024-03-11 20:32:50 -0700

    Added Image to Video 1-1, added Reserver Memory data, export data to CSV

--- run_bench.py
+++ run_bench.py
@@ -2,6 +2,9 @@ from huggingface_hub import snapshot_download
 from subprocess import run
 import re
 import os
+import csv
+
+access_token = "ACCESS_TOKEN_HERE"
 
 # Get the absolute path of the current directory
 current_directory = os.getcwd()
@@ -43,9 +46,9 @@ listOfBenchmarks = ['"docker", "run", "-v", "' + current_directory + '/models:/m
                     '"docker", "run", "-v", "' + current_directory + '/models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-image", "--model_id", "prompthero/openjourney-v4", "--runs", "3"',
                     '"docker", "run", "-e", "SFAST=true", "-v", "' + current_directory + '/models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-image", "--model_id", "prompthero/openjourney-v4", "--runs", "3"',
                     '"docker", "run", "-v", "' + current_directory + '/models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-video", "--model_id", "stabilityai/stable-video-diffusion-img2vid-xt", "--runs", "3"',
-                    '"docker", "run", "-e", "SFAST=true", "-v", "' + current_directory + '/models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-video", "--model_id", "stabilityai/stable-video-diffusion-img2vid-xt", "--runs", "3"']
-                    #'"docker", "run", "-v", "./models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-video", "--model_id", "stabilityai/stable-video-diffusion-img2vid-xt-1-1", "--runs", "3"',
-                    #'"docker", "run", "-e", "SFAST=true", "-v", "./models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-video", "--model_id", "stabilityai/stable-video-diffusion-img2vid-xt-1-1", "--runs", "3"']
+                    '"docker", "run", "-e", "SFAST=true", "-v", "' + current_directory + '/models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-video", "--model_id", "stabilityai/stable-video-diffusion-img2vid-xt", "--runs", "3"',
+                    '"docker", "run", "-v", "' + current_directory + '/models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-video", "--model_id", "stabilityai/stable-video-diffusion-img2vid-xt-1-1", "--runs", "3"',
+                    '"docker", "run", "-e", "SFAST=true", "-v", "' + current_directory + '/models:/models", "livepeer/ai-runner:latest", "python", "bench.py", "--pipeline", "image-to-video", "--model_id", "stabilityai/stable-video-diffusion-img2vid-xt-1-1", "--runs", "3"']
 
 
 def getGPUCard():
@@ -71,7 +74,6 @@ def getGPUCard():
 
 
 def downloadAllModels():
-    #if not os.path.exists("models/models--stabilityai--sd-turbo"):
     print("Downloading model... models--stabilityai--sd-turbo - This may take a few minutes.")
     try:
         snapshot_download(repo_id="stabilityai/sd-turbo", allow_patterns=["*.fp16.safetensors", "*.json", "*.txt"], ignore_patterns=[".onnx", ".onnx_data"], cache_dir="models")
@@ -120,14 +122,13 @@ def downloadAllModels():
         print("An error occurred while downloading the model. Attempting to continue...")
     print("Download complete!")
 
-    #if not os.path.exists("models/models--stabilityai--stable-video-diffusion-img2vid-xt-1-1"):
-    #    print("Downloading model... smodels--stabilityai--stable-video-diffusion-img2vid-xt - This may take a few minutes.")
-    #    try:
-    #        snapshot_download(repo_id="stabilityai/stable-video-diffusion-img2vid-xt-1-1", allow_patterns=["*.fp16.safetensors", "*.json"], token="$HF_TOKEN", cache_dir="models")
-    #    except Exception as e:
-    #        print(e)
-   #         print("An error occurred while downloading the model. Attempting to continue...")
-    #    print("Download complete!")
+    print("Downloading model... smodels--stabilityai--stable-video-diffusion-img2vid-xt-1-1 - This may take a few minutes.")
+    try:
+        snapshot_download(repo_id="stabilityai/stable-video-diffusion-img2vid-xt-1-1", allow_patterns=["*.fp16.safetensors", "*.json"], token=access_token, cache_dir="models")
+    except Exception as e:
+        print(e)
+        print("An error occurred while downloading the model. Attempting to continue...")
+    print("Download complete!")
 
     print("All models downloaded in the models folder.")
 
@@ -144,14 +145,17 @@ def runBenchmark(command, card, pause):
     # Get Regular expression of "avg inference time:" from stout
     tempInferenceTime = re.findall(r'avg inference time: (.+?)s\n', benchmark.stdout)
     tempMaxMemory = re.findall(r'avg inference max GPU memory allocated: (.+?)GiB\n', benchmark.stdout)
+    tempMaxReserveMemory = re.findall(r'avg inference max GPU memory reserved: (.+?)GiB\n', benchmark.stdout)
     if tempInferenceTime != []:
         print(benchmark.stdout)
         try:
             avgInferenceTime = tempInferenceTime[0]
             maxGPUMemoryAllocated = tempMaxMemory[0]
+            maxGPUMemoryReserved = tempMaxReserveMemory[0]
         except:
             avgInferenceTime = '0'
             maxGPUMemoryAllocated = '0'
+            maxGPUMemoryReserved = '0'
 
     else:
         print(benchmark.stdout)
@@ -160,9 +164,11 @@ def runBenchmark(command, card, pause):
         if tempMemoryError != []:
             avgInferenceTime = 'out of memory'
             maxGPUMemoryAllocated = 'out of memory'
+            maxGPUMemoryReserved = 'out of memory'
         else:
             avgInferenceTime = '0'
             maxGPUMemoryAllocated = '0'
+            maxGPUMemoryReserved = '0'
 
 
     # Open a file in write mode and write the output
@@ -173,7 +179,7 @@ def runBenchmark(command, card, pause):
         file.write(benchmark.stdout)
         if tempInferenceTime == []:
             file.write(benchmark.stderr)
-    return avgInferenceTime, maxGPUMemoryAllocated
+    return avgInferenceTime, maxGPUMemoryAllocated, maxGPUMemoryReserved
 
 
 def pullLatestDockerImage():
@@ -184,6 +190,7 @@ def pullLatestDockerImage():
 if __name__ == "__main__":
     avg_inference_time = []
     max_GPU_memory_allocated = []
+    max_GPU_memory_reserved = []
     card = getGPUCard()
     print("Benchmarks are heavy and will pause between each benchmark to ensure no errors occur. Only skip if you are sure.")
     pause = input("Skip pausing between benchmarks? (y/n): ")
@@ -195,17 +202,29 @@ if __name__ == "__main__":
         file.write("GPU Slot: " + card + "\n")
         file.write("===================================================================================================== \n")
     for command in listOfBenchmarks:
-        inferenceTime, GPUMemory = runBenchmark(command, card, pause)
+        inferenceTime, GPUMemory, GPUReserve = runBenchmark(command, card, pause)
         avg_inference_time.append(inferenceTime)
         max_GPU_memory_allocated.append(GPUMemory)
+        max_GPU_memory_reserved.append(GPUReserve)
         with open('results.txt', 'a') as file:
             file.write("===================================================================================================== \n")
             file.write("Average Inference Time: " + str(avg_inference_time) + "\n")
             file.write("Max GPU Memory Allocated: " + str(max_GPU_memory_allocated) + "\n")
+            file.write("Max GPU Memory Reserved: " + str(max_GPU_memory_reserved) + "\n")
             file.write("===================================================================================================== \n")
+        csvData = [["Row 1 - Average Inference Time", "Row 2 - Max GPU Memory Allocated", "Row 3 - Max GPU Memory Reserved"],
+                   avg_inference_time,
+                   max_GPU_memory_allocated,
+                   max_GPU_memory_reserved]
+        with open('results.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            # Write each row to the file
+            for row in csvData:
+                writer.writerow(row)
         print("=====================================================================================================")
         print("Average Inference Time: ", avg_inference_time)
         print("Max GPU Memory Allocated: ", max_GPU_memory_allocated)
+        print("Max GPU Memory Reserved: ", max_GPU_memory_reserved)
         print("=====================================================================================================")
         print("Benchmark complete. Results saved to results.txt - Moving on to the next benchmark.")
         if pause != "y":
